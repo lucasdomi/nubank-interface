1bc756730e86efe4df327960245301db79c809fb
Lucas Domingues <lucasdomi@globo.com>
2019-05-11 11:11:26 -0300

    feat: Adicionando pasta de componentes

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { View } from 'react-native';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      {/* <Top>
+        <Logo source={} />
+        <Title>Lucas</Title>
+      </Top> */}
+    </Container>
+  );
+}
