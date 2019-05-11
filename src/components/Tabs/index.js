85292b0328fbb46cc16763d0177dddae234909c7
Lucas Domingues <lucasdomi@globo.com>
2019-05-11 13:25:45 -0300

    feat: adicionando tabs na interface

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,10 +1,34 @@
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
 // import { View } from 'react-native';
 
-import { Container } from './styles';
+import { Container, TabsContainer, TabItem, TabText } from './styles';
 
 export default function Tabs() {
   return (
-    <Container />
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#FFF" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#FFF" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#FFF" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#FFF" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
   );
 }
