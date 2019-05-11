413b686e7b8e3c6ddc7d6bcbc172682c4bfe06c1
Lucas Domingues <lucasdomi@globo.com>
2019-05-11 11:29:47 -0300

    feat: adicionando header na aplicacao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 // import { View } from 'react-native';
-
-import { Container } from './styles';
+import { Container, Top, Title, Logo } from './styles';
+import logo from "../../assets/Nubank_Logo.png"
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export default function Header() {
   return (
     <Container>
-      {/* <Top>
-        <Logo source={} />
+      <Top>
+        <Logo source={logo} />
         <Title>Lucas</Title>
-      </Top> */}
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
     </Container>
   );
 }
