84b97851df80b8a65e47688ec11cbe0332c1708c
Lucas Domingues <lucasdomi@globo.com>
2019-05-13 21:32:52 -0300

    feat: adicionando animacao de arrastar o card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -11,6 +11,8 @@ import Menu from '~/components/Menu';
 import Tabs from '~/components/Tabs';
 
 export default function Main() {
+  let offset = 0; 
+
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -25,7 +27,29 @@ export default function Main() {
   );
 
   function onHandlerStateChange(event) {
-
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+      offset += translationY;
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+      // translateY.setOffset(offset);
+      // translateY.setValue(0);
+    }
   }
 
   return (
